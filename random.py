46b417a049f3cb1b840bc204396d1646a6d24158
Busari Moruf <busarimoruf20@gmail.com>
2025-09-02 05:43:02 -0700

    new changes

--- random.py
+++ random.py
@@ -1,4 +1,6 @@
+
+
 #******************* Learning Phase 1 ***************************
-print('Develop'[-4:-1])
-print('DataScience'[-1:-5:-1])
-print('Develop'[1::2])
+# print('Develop'[-4:-1])
+# print('DataScience'[-1:-5:-1])
+# print('Develop'[1::2])
