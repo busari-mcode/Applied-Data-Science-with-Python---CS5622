f5cbb3bcec039845e5bb47cc332c9ff5d91bc60a
Busari Moruf <busarimoruf20@gmail.com>
2025-09-02 14:16:16 -0700

    new changes

--- random.py
+++ random.py
@@ -1,6 +1,8 @@
 
 
 #******************* Learning Phase 1 ***************************
+# my_dict = {'key1':123, 'key2':[12,23,33], 'key3':['item0','item1','item2']}
+# print(my_dict['key3'][0].upper())
 # print('Develop'[-4:-1])
 # print('DataScience'[-1:-5:-1])
 # print('Develop'[1::2])
