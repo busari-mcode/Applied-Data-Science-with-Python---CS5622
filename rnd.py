1e42047f7877b132afdf295df7b00678d999c8a9
Busari Moruf <busarimoruf20@gmail.com>
2025-09-06 08:17:58 -0700

    new changes

--- rnd.py
+++ rnd.py
@@ -1,4 +1,41 @@
 #******************* Learning Phase 2 ***************************
+# list6 = [3, 66, 70, 25, 43, 29, 100, 50, 88, 28, 96]
+
+# total = 0
+
+# for num in list6:
+#     if num == 100:
+#         break    
+#     total += num
+
+# print(total)
+
+#******************
+# month = 'May'
+
+# if month in ("January", "March", "May", "July", "August", "October", "December"):
+#     print(month,"has 31 days")
+# elif month in ("April", "June", "September", "November"):
+#     print(month,"has 30 days")
+# else:
+#     print(month,"has 28 or 29 days")
+
+
+#*******************
+# word = input("Enter one word: ")
+
+# if word == 'hi':
+#     print('Hi to you too!')
+# elif word == 'hello':
+#     print('Hello hello')
+# elif word == 'howdy':
+#     pirnt('Howdyyy!')
+# elif word == 'hey':
+#     print('Hey hey hey!')
+# else:
+#     print("I don't know what this word means")
+
+#***************
 # s5 = 'Objects in Python can have built-in methods.'
 # s6 = s5.replace('Objects in Python can have built-in methods.', 'Objects in Python can work with built-in methods.')
 
